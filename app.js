d992744d35455c82b0d7289de158d1a0ef1c929f
anandtp00 <anandtp00@gmail.com>
2020-05-17 19:17:02 +0530

    error handling

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const request = require('postman-request');
 
-
+// 10.9760,76.2254
 
 const url = "http://api.weatherstack.com/current?access_key=69b7de896864a71546caba156130aaa6&query=10.9760,76.2254";
 
@@ -8,14 +8,21 @@ const url1 = "https://api.mapbox.com/geocoding/v5/mapbox.places/Perintalmanna.js
 
 
 request({ url: url, json: true }, (error, response) => {
+    if (response.body.error) {
+        console.log(response.body.error.info);
+    }
+    else if (!error) {
+        console.log("Current Weather description : " + response.body.current.weather_descriptions[0]);
 
-    console.log("Current Weather description : " + response.body.current.weather_descriptions[0]);
+        console.log("It is currently " + response.body.current.temperature + " degrees out. ");
 
-    console.log("It is currently " + response.body.current.temperature + " degrees out. ");
+        console.log("It feels like " + response.body.current.precip + "% chance to rain. ");
 
-    console.log("It feels like " + response.body.current.precip + "% chance to rain. ");
+        console.log("It feels like " + response.body.current.feelslike + " degrees out ");
+    } else {
+        console.log("Error in accessing the weather API " + error);
 
-    console.log("It feels like " + response.body.current.feelslike + " degrees out ");
+    }
     // if (!error) {
     //     const data = JSON.parse(response.body);
     //     console.log(data.current);
@@ -26,6 +33,17 @@ request({ url: url, json: true }, (error, response) => {
 });
 
 request({ url: url1, json: true }, (error, response) => {
-    console.log("Latitude = "+response.body.features[0].geometry.coordinates[0]);
-    console.log("Longitude = "+response.body.features[0].geometry.coordinates[1]);
+
+    const length = response.body.features.length;
+    
+
+    if (error) {
+        console.log("Error in accessing the geocoding API " + error);
+    }
+    else if(length === 0){
+        console.log()
+    }else {
+        console.log("Latitude = " + response.body.features[0].geometry.coordinates[0]);
+        console.log("Longitude = " + response.body.features[0].geometry.coordinates[1]);
+    }
 })
