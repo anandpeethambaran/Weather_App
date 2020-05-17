0e45ea65a4877d2fab53e33dd7caf5dde0434f91
anandtp00 <anandtp00@gmail.com>
2020-05-17 13:49:00 +0530

    accessing the data from the json response from API

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const request = require('postman-request');
 
 
 
-const url = "http://api.weatherstack.com/current?access_key=69b7de896864a71546caba156130aaa6&query=11.0510,76.0711";
+const url = "http://api.weatherstack.com/current?access_key=69b7de896864a71546caba156130aaa6&query=10.9760,76.2254";
 
 
-request(url,(error,response)=>{
-    if(!error){
-        const data = JSON.parse(response.body);
-        console.log(data.current);
-    }
-    else{
-        console.log("Error in accessing the url");
-    }
+request({ url: url, json: true }, (error, response) => {
+    console.log("It is currently "+response.body.current.temperature+" degrees out. ");
+    
+    console.log("It feels like "+response.body.current.precip+"% chance to rain. ");
+
+    console.log("It feels like "+response.body.current.feelslike+" degrees out ");
+    // if (!error) {
+    //     const data = JSON.parse(response.body);
+    //     console.log(data.current);
+    // }
+    // else {
+    //     console.log("Error in accessing the url");
+    // }
 })
